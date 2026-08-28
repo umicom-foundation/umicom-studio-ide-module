cade85ec61281e7bc9e4b0551b6c49b06a28ddec
Sammy Hegab <Sammy.Hegab@outlook.com>
2026-08-28 01:29:29 +0100

    fix(studio): adopt unambiguous frontend flags

--- src/productisation_contribution.c
+++ src/productisation_contribution.c
@@ -18,7 +18,7 @@ static const UmiProductApplicationAdoption ADOPTION = {
     "org.umicom.studio",
     "Umicom Studio IDE",
     "umicom-studio-ide",
-    UMI_PRODUCT_FRONTEND_CONSOLE | UMI_PRODUCT_FRONTEND_GTK4,
+    UMI_PRODUCT_FRONTEND_FLAG_CONSOLE | UMI_PRODUCT_FRONTEND_FLAG_GTK4,
     1,
     1,
     1,
